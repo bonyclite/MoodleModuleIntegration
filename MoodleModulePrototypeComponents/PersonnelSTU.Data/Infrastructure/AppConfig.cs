f64f1cbaa18ccb5459f1ec4cc9949bc6c0d3220b
DESKTOP-2SCK5R3\vladi <vladislav-korotaev-97@mail.ru>
2018-02-12 20:52:28 +0700

    Created PersonnelSTU.Data project (dll). Added AppConfig which provides several data: connection strings, names for fake and real databases.

---
+++ MoodleModulePrototypeComponents/PersonnelSTU.Data/Infrastructure/AppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace PersonnelSTU.Data.Infrastructure
+{
+    public static class AppConfig
+    {
+        public static string FakePersonnelSTUDbName => ConfigurationManager.AppSettings["FakePersonnelSTUDb"];
+
+        public static string PersonnelSTUDbName => ConfigurationManager.AppSettings["PersonnelSTUDbName"];
+
+        public static string FakePersonnelSTUDbConnectionString => ConfigurationManager.ConnectionStrings["FakePersonnelSTUDb"].ConnectionString;
+    }
+}
