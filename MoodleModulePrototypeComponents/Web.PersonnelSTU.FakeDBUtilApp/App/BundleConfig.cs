8a8cf69581e01c70d4dbb5cc99e64847051b6e2f
DESKTOP-2SCK5R3\vladi <vladislav-korotaev-97@mail.ru>
2018-02-17 22:27:44 +0700

    Removed unused javascript frameworks and bootstrap styles.

--- MoodleModulePrototypeComponents/Web.PersonnelSTU.FakeDBUtilApp/App/BundleConfig.cs
+++ MoodleModulePrototypeComponents/Web.PersonnelSTU.FakeDBUtilApp/App/BundleConfig.cs
@@ -8,24 +8,6 @@ namespace Web.PersonnelSTU.FakeDBUtilApp
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles()
         {
-            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            BundleTable.Bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
