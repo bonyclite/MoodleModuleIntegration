ceab0388b7054bc7d7f617c62f607ce49fcbb064
DESKTOP-2SCK5R3\vladi <vladislav-korotaev-97@mail.ru>
2018-02-17 21:30:06 +0700

    Modified App.Config, added Web.Common for web apps implementation, created AppStart file for further injections, removed autofac references in Common.

---
+++ MoodleModulePrototypeComponents/Web.Common/App/AppStart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Integration.Mvc;
+using Common;
+
+namespace Web.Common.App
+{
+    public class AppStart
+    {
+        public Func<IRegistrationBuilder<object, object, object>, IRegistrationBuilder<object, object, object>> LifetimeScopeConfigurator { get; }
+
+        public AppStart(Func<IRegistrationBuilder<object, object, object>, IRegistrationBuilder<object, object, object>> lifetimeScopeConfigurator = null)
+        {
+            LifetimeScopeConfigurator = lifetimeScopeConfigurator ?? (registrationBuilder => registrationBuilder.InstancePerRequest());
+        }
+
+        public virtual void Run()
+        {
+            var builder = new ContainerBuilder();
+            RegisterTypes(builder);
+
+            builder.RegisterModule(new AutofacWebTypesModule());
+
+            RegisterRoutes();
+            RegisterBundles();
+
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            var container = builder.Build();
+            SetDependencyResolver(container);
+        }
+
+        public virtual void RegisterTypes(ContainerBuilder builder)
+        {
+            PersonnelSTU.Data.Bootstrapper.RegisterTypes(builder, LifetimeScopeConfigurator);
+        }
+
+        protected virtual void RegisterBundles()
+        {
+        }
+
+        protected virtual void RegisterRoutes()
+        {
+        }
+
+        protected virtual void SetDependencyResolver(IContainer container)
+        {
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            ServiceLocator.Register(serviceType => DependencyResolver.Current.GetService(serviceType));
+        }
+    }
+}
