ca269afe874d89cb665dd95e15fc481399c9bd1f
DESKTOP-2SCK5R3\vladi <vladislav-korotaev-97@mail.ru>
2018-02-22 22:02:55 +0700

    Remove EF from PersonnelSTU.Data, implemented infrasture for data access layer.

---
+++ MoodleModulePrototypeComponents/Common.Data/Infrastructure/DbContextAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Common.Data.Infrastructure
+{
+    public interface IDbContext
+    {
+        EntityState GetEntityState<TEntity>(TEntity entity) where TEntity : class;
+        TEntity AttachEntity<TEntity>(TEntity entity, EntityState? withState = null) where TEntity : class;
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        void SaveChanges();
+    }
+
+    public class DbContextAdapter : IDbContext
+    {
+        private readonly DbContext _dbContext;
+
+        public DbContextAdapter(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public EntityState GetEntityState<TEntity>(TEntity entity) where TEntity : class
+        {
+            return _dbContext.Entry(entity).State;
+        }
+
+        public TEntity AttachEntity<TEntity>(TEntity entity, EntityState? withState = null) where TEntity : class
+        {
+            var attachedEntity = _dbContext.Set<TEntity>().Attach(entity);
+            if (withState.HasValue)
+                _dbContext.Entry(entity).State = withState.Value;
+
+            return attachedEntity;
+        }
+
+        public DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return _dbContext.Set<TEntity>();
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ MoodleModulePrototypeComponents/Common.Data/Infrastructure/DbFactoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Data.Infrastructure
+{
+    public interface IDbFactory
+    {
+        IDbContext Get();
+    }
+
+    public abstract class DbFactoryBase : IDbFactory
+    {
+        private IDbContext _dbContext;
+
+        public IDbContext Get()
+        {
+            return _dbContext ?? (_dbContext = Create());
+        }
+
+        protected abstract IDbContext Create();
+    }
+}
