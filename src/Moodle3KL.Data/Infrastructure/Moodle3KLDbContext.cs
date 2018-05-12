30b90de48380db0e41ea057dc72d59b9f6be1b2a
Vlad Korotaev <korotaev@magora-systems.com>
2018-05-12 12:55:29 +0700

    Generated domain models in Moodle3KL.Data.

---
+++ src/Moodle3KL.Data/Infrastructure/Moodle3KLDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+
+namespace Moodle3KL.Data.Infrastructure
+{
+    public class Moodle3KLDbContext : DbContext
+    {
+    }
+}
---
+++ src/Moodle3KL.Data/Infrastructure/Moodle3KLDbFactory.cs
@@ -0,0 +1,17 @@
+﻿using Common.Data.Infrastructure;
+
+namespace Moodle3KL.Data.Infrastructure
+{
+    public interface IMoodle3KLDbFactory : IDbFactory
+    {
+
+    }
+
+    public class Moodle3KLDbFactory : IMoodle3KLDbFactory
+    {
+        public IDbContext Get()
+        {
+            return new DbContextAdapter(new Moodle3KLDbContext());
+        }
+    }
+}
